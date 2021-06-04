a339a27bd1a6b6b0c79003beb111be5db3191cee
Süleyman Sezer <me.iamcodder@gmail.com>
2021-06-04 18:58:11 +0300

    1604th question solved

---
+++ src/Leetcode/easy/1604.kt
@@ -0,0 +1,30 @@
+package Leetcode.easy
+
+
+
+class ParkingSystem(big: Int, medium: Int, small: Int) {
+
+    val map = HashMap<Int,Int>()
+    init {
+        map[1] = big
+        map[2] = medium
+        map[3] = small
+    }
+
+    fun addCar(carType: Int): Boolean {
+        val carSize = map[carType]
+        carSize?.let {
+            return if(carSize>0) {
+                map[carType]=it-1
+                true
+            }else false
+        }
+        return false
+    }
+}
+
+private fun main() {
+    val system = ParkingSystem(1,1,0)
+    system.addCar(2)
+
+}
